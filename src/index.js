273342d6e28d171bcab4d56fbc8c5bc32976a0ba
anotherFeng <regnagleppod@gmail.com>
2018-06-22 15:45:18 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ $(window).on("load", function(){
   $(".loader .inner").fadeOut(750, function(){
     $(".loader").fadeOut(1000);
   });
+  $(".items").isotope({
+    filter: '*',
+    animationOptions: {
+        duration: 1500,
+        easing: 'linear',
+        queue: false
+    }
+ });
 })
 
 $(document).ready(function(){

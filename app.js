8d4a52db5db3c6b91043b604d0a4c0d7802a6b3f
anotherFeng <regnagleppod@gmail.com>
2018-06-22 04:47:52 -0400

    changes

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.set('view engine')
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/index.html'));
+app.use('/', (req, res)=>{
+  res.render('index');
+})
+
 app.listen(port, ()=>{
   console.log("listening");
 })
